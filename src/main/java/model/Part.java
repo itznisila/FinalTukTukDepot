18e7fd653065ff814c2878309b5dea9e14fcae0d
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-22 22:45:21 +0530

    called to the constructor and connected variables with this keyword in Part.java

--- src/main/java/model/Part.java
+++ src/main/java/model/Part.java
@@ -12,4 +12,16 @@ public class Part {
     private String category;
     private LocalDate dateAdded;
     private String imagePath;
+
+    public Part(String code, String name, String dealerName, double price,
+                int quantity, String category, LocalDate dateAdded, String imagePath) {
+        this.code = code;
+        this.name = name;
+        this.dealerName = dealerName;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.dateAdded = dateAdded;
+        this.imagePath = imagePath;
+    }
 }
