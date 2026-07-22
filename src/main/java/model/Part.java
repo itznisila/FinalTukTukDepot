93bab86082f2d15071b5c1f346c6587604864726
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-22 22:35:17 +0530

    Created variables in Part.java as private so no one can access to that from outside class

---
+++ src/main/java/model/Part.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Part {
+
+    private String code;
+    private String name;
+    private String dealerName;
+    private double price;
+    private int quantity;
+    private String category;
+    private LocalDate dateAdded;
+    private String imagePath;
+}
