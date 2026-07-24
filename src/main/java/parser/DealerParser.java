f19f8a0208661b8de1cff6a224e903f50e78283b
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-24 19:59:33 +0530

    DealerParser.java - created DealerParser class and read the file using BufferedReader

---
+++ src/main/java/parser/DealerParser.java
@@ -0,0 +1,24 @@
+package parser;
+import model.Dealer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DealerParser {
+    public List<Dealer> parseFile(String filePath) {
+        List<Dealer> dealers = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Dealer dealer = parseLine(line);
+                dealers.add(dealer);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return dealers;
+    }
+}
