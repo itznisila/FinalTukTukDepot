a84db3fccd3212fff9deedbc7d5ed5f47260b589
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-26 22:41:08 +0530

    Completed LowStockMonitor class

--- src/main/java/logic/LowStockMonitor.java
+++ src/main/java/logic/LowStockMonitor.java
@@ -1,4 +1,20 @@
 package logic;
 
+import model.Part;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LowStockMonitor {
+
+    public static List<Part> getLowStockItems(List<Part> parts) {
+        List<Part> lowStock = new ArrayList<>();
+
+        for (Part p : parts) {
+            if (p.getQuantity() < p.getLowStockThreshold()) {
+                lowStock.add(p);
+            }
+        }
+
+        return lowStock;
+    }
 }
