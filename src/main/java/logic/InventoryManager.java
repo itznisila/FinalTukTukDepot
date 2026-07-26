8372ef8d4967e1a810d07b5fb3f225569fc98ce2
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-26 11:56:43 +0530

    Created InventoryManager.java with empty parts list

---
+++ src/main/java/logic/InventoryManager.java
@@ -0,0 +1,15 @@
+package logic;
+
+import model.Part;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventoryManager {
+
+    private List<Part> parts;
+    public InventoryManager() {
+        this.parts = new ArrayList<Part>();
+    }
+
+}
