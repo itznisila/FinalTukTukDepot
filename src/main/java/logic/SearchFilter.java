cd1c843f9ddd6450f53c00c9b1c871a2d3887e2d
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-26 22:27:46 +0530

    For loop for filter

--- src/main/java/logic/SearchFilter.java
+++ src/main/java/logic/SearchFilter.java
@@ -1,4 +1,16 @@
 package logic;
 
+import model.Part;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFilter {
+    public static List<Part> search(List<Part> parts, String category,
+                                    Double minPrice, Double maxPrice, String keyword) {
+        List<Part> results = new ArrayList<>();
+
+        for (int i = 0; i < parts.size(); i++) {
+            Part p = parts.get(i);
+            boolean matches = true;
 }
