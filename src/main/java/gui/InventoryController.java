89534250095cc625d9952e4a135694841be5e628
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-27 12:29:34 +0530

    InventoryController.java Table

--- src/main/java/gui/InventoryController.java
+++ src/main/java/gui/InventoryController.java
@@ -2,6 +2,7 @@ package gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import model.Part;
 
 public class InventoryController {
 
@@ -9,3 +10,12 @@ public class InventoryController {
     @FXML private TextField minPriceField;
     @FXML private TextField maxPriceField;
     @FXML private TextField keywordField;
+
+    @FXML private TableView<Part> inventoryTable;
+    @FXML private TableColumn<Part, String> codeColumn;
+    @FXML private TableColumn<Part, String> nameColumn;
+    @FXML private TableColumn<Part, Double> priceColumn;
+    @FXML private TableColumn<Part, Integer> quantityColumn;
+    @FXML private TableColumn<Part, String> categoryColumn;
+    @FXML private TableColumn<Part, Integer> thresholdColumn;
+    @FXML private TableColumn<Part, String> imageColumn;
