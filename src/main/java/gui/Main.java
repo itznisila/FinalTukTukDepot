f69e45525535b647141f7ff7260b108af4890c1a
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-28 01:54:54 +0530

    Updated Main

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -4,13 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainTabs.fxml"));
-        Scene scene = new Scene(root, 900, 650);
+
+        FXMLLoader inventoryLoader = new FXMLLoader(getClass().getResource("/fxml/Inventory.fxml"));
+        Parent inventoryRoot = inventoryLoader.load();
+        InventoryController inventoryController = inventoryLoader.getController();
+
+        FXMLLoader dealersLoader = new FXMLLoader(getClass().getResource("/fxml/Dealers.fxml"));
+        Parent dealersRoot = dealersLoader.load();
+
+        FXMLLoader cartLoader = new FXMLLoader(getClass().getResource("/fxml/Cart.fxml"));
+        Parent cartRoot = cartLoader.load();
+        CartController cartController = cartLoader.getController();
+
+        cartController.setInventoryManager(inventoryController.getInventoryManager());
+        cartController.setInventoryController(inventoryController);
+
+        Tab inventoryTab = new Tab("Inventory", inventoryRoot);
+        Tab dealersTab = new Tab("Dealers", dealersRoot);
+        Tab cartTab = new Tab("Cart", cartRoot);
+        inventoryTab.setClosable(false);
+        dealersTab.setClosable(false);
+        cartTab.setClosable(false);
+
+        TabPane tabPane = new TabPane(inventoryTab, dealersTab, cartTab);
+
+        Scene scene = new Scene(tabPane, 900, 650);
         primaryStage.setTitle("Malabe Tuk Tuk Spares Depot");
         primaryStage.setScene(scene);
         primaryStage.show();
