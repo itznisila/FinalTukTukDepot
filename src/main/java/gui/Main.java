5b1cc4b3895905ecd1b20664016f8f1dc7b20497
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-27 23:56:44 +0530

    Fixed errors

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,18 +1,21 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-    public void start(Stage stage) {
-        Scene scene = new Scene(new Label("Spares Depot"), 400, 300);
-        stage.setTitle("Malabe Tuk-Tuk & Three-Wheeler Spares Depot");
-        stage.setScene(scene);
-        stage.show();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainTabs.fxml"));
+        Scene scene = new Scene(root, 900, 650);
+        primaryStage.setTitle("Malabe Tuk Tuk Spares Depot");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
