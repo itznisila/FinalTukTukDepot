79b1b19c94f564675df441ea84c7410b3e5ff81f
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-28 00:41:20 +0530

    Cart.java null part

--- src/main/java/logic/Cart.java
+++ src/main/java/logic/Cart.java
@@ -1,4 +1,40 @@
 package logic;
 
+import model.Part;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
-}
+
+    public static class CartLine {
+        private Part part;
+        private int quantity;
+
+        public CartLine(Part part, int quantity) {
+            this.part = part;
+            this.quantity = quantity;
+        }
+
+        public Part getPart() { return part; }
+        public int getQuantity() { return quantity; }
+        public void setQuantity(int quantity) { this.quantity = quantity; }
+    }
+
+    private List<CartLine> lines;
+
+    public Cart() {
+        this.lines = new ArrayList<>();
+    }
+
+    public List<CartLine> getLines() {
+        return lines;
+    }
+
+    public boolean addToCart(Part p, int qty) {
+        if (p == null) {
+            System.out.println("Cannot add to cart: part is null");
+            return false;
+        }
+
+    }
