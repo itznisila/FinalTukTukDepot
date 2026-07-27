4f05ea8a6b7e5cc1bd1540429a0f74b40aabc93a
Nisila Dinod <nisilaudayasiri11@gmail.com>
2026-07-27 12:28:47 +0530

    InventoryController.java Search bar

---
+++ src/main/java/gui/InventoryController.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class InventoryController {
+
+    @FXML private TextField searchCategoryField;
+    @FXML private TextField minPriceField;
+    @FXML private TextField maxPriceField;
+    @FXML private TextField keywordField;
